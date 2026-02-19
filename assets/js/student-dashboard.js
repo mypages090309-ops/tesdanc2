10d692d5b3b90bb2a4c687e404d574d5e8e324ca
mypages090309-ops <mypages.090309@gmail.com>
2026-02-19 10:45:23 +0800

    Add files via upload

---
+++ assets/js/student-dashboard.js
@@ -0,0 +1,5 @@
+// Basic logout simulation
+document.getElementById("logoutBtn").addEventListener("click", function(){
+  alert("Logging out...");
+  window.location.href = "login.html";
+});
---
+++ assets/js/trainer-dashboard.js
@@ -0,0 +1,4 @@
+document.getElementById("logoutBtn").addEventListener("click", function(){
+  alert("Logging out...");
+  window.location.href = "login.html";
+});
